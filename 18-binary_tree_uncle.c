8b209b99bbab4caf3cab9d828994ad1d4097aa77
AliMoussa-00 <elmoussaouiali0065@gmail.com>
2023-11-29 03:08:34 -0800

    fix .18

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -8,7 +8,8 @@
 
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
-	if (!node || (node && node->parent && !node->parent->parent))
+	if (!node || (node && !node->parent) ||
+			(node && node->parent && !node->parent->parent))
 		return (NULL);
 
 	if (node->parent->parent->left == node->parent)
